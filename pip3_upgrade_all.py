b13a3baddea73ac574f808d48f5fc538b71c901f
yanwei <theoneyw@gmail.com>
2020-02-05 18:23:31 +0800

    using python instead of python3

--- pip3_upgrade_all.py
+++ pip3_upgrade_all.py
@@ -6,19 +6,19 @@
 
 2017-11-27, yanwei, pip 9.0.1
 2018-05-05, yanwei, support pip 10.0.1
-2020-02-05, yanwei, replace pip3 with 'python3 -m pip'
+2020-02-05, yanwei, replace pip3 with 'python -m pip'
 """
 
 import json
 import os
 
 package_list = json.loads(
-    os.popen('python3 -m pip list --outdated --format json').readlines()[0])
+    os.popen('python -m pip list --outdated --format json').readlines()[0])
 
 updated_packages = []
 
 for package in package_list:
-    os.system('python3 -m pip install -U ' + package['name'])
+    os.system('python -m pip install -U ' + package['name'])
     updated_packages.append(package['name'])
 
 if len(updated_packages) == 0:
